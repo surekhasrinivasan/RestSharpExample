549b159aa8208b710ad17241ec49b46d6b12487c
surekhasrinivasan <surekhasrao@gmail.com>
2018-11-22 06:38:08 -0500

    Scraper project using RestSharp

--- RestSharpExample/Program.cs
+++ RestSharpExample/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 
 namespace RestSharpExample
 {
@@ -10,6 +15,14 @@ namespace RestSharpExample
     {
         static void Main(string[] args)
         {
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("/users", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+
+            var jsonArray = JArray.FromObject(JsonConvert.DeserializeObject(content));                   
+            Console.WriteLine(jsonArray);
         }
     }
 }
